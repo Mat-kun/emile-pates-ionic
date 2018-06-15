d1b887e84d668b898f37ae41434f4ff8e25c6d56
Colin Decomis <defdruil@gmail.com>
2018-06-15 15:27:06 +0200

    page shopping list  + esition ingredient shopping list ok

--- src/pages/edit-ingredient/edit-ingredient.ts
+++ src/pages/edit-ingredient/edit-ingredient.ts
@@ -3,6 +3,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {Ingredient} from '../../models/ingredient';
 import { FridgePage } from '../fridge/fridge';
 import { FridgeService } from '../../services/implementations/fridge.service'
+import { ShoppingListService } from '../../services/implementations/shopping-list.service';
+import { ShoppingList } from '../../models/shopping-list';
 
 /**
  * Generated class for the EditIngredientPage page.
@@ -20,29 +22,47 @@ export class EditIngredientPage implements OnInit {
 
   // ingredientToEdit:Ingredient;
   ingredientToEdit = {id: 0, name: '', quantity: 0, unity: '', peremptionDate: new Date()};
-  
   editionMode: boolean;
+  shoppingListId: number;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public fridgeService: FridgeService) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams, 
+    public fridgeService: FridgeService,
+    public shoppingListService: ShoppingListService
+  
+  ) {
   }
 
   ngOnInit(){
+    this.shoppingListId = -1
     this.ingredientToEdit = this.navParams.data.ingredient;
     if(this.navParams.data.type == "edit"){
       this.editionMode = true;
     }else{
       this.editionMode = false;
     }
+    if (this.navParams.data.shoppingListId !== undefined && this.navParams.data.shoppingListId !== -1){
+      this.shoppingListId = this.navParams.data.shoppingListId;
+    }
   }
 
   register(){
-    if(this.editionMode){
-      this.fridgeService.editIngredient(this.ingredientToEdit);          
-    }else{
-      this.fridgeService.addIngredient(this.ingredientToEdit);
-      
+    console.log(this.shoppingListId);
+    if (this.shoppingListId === -1){
+      if(this.editionMode){
+        this.fridgeService.editIngredient(this.ingredientToEdit);          
+      }else{
+        this.fridgeService.addIngredient(this.ingredientToEdit);
+      }
+    } else {
+      if(this.editionMode){
+        throw new Error("Not Implemented");
+      }else{
+        this.shoppingListService.getShoppingListById(this.shoppingListId).ingredientList.push(this.ingredientToEdit);
+      }
     }
-    this.navCtrl.push(FridgePage);
+    this.navCtrl.pop();
   }
 
   cancel(){
--- src/pages/edit-shopping-list/edit-shopping-list.ts
+++ src/pages/edit-shopping-list/edit-shopping-list.ts
@@ -1,5 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ShoppingList } from '../../models/shopping-list';
+import { Ingredient } from '../../models/ingredient';
+import { FridgeService } from '../../services/implementations/fridge.service';
+import { ShoppingListService } from '../../services/implementations/shopping-list.service';
+import { EditIngredientPage } from '../edit-ingredient/edit-ingredient';
+import { IngredientService } from '../../services/implementations/ingredient.service';
 
 /**
  * Generated class for the EditShoppingListPage page.
@@ -13,13 +19,69 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   selector: 'page-edit-shopping-list',
   templateUrl: 'edit-shopping-list.html',
 })
-export class EditShoppingListPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+export class EditShoppingListPage implements OnInit{
+  shoppingListToEdit: ShoppingList;
+  editionMode: boolean;
+  ingredientsToAdd: Ingredient[];
+  
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public fridgeService: FridgeService,
+    public shoppingListService: ShoppingListService,
+  ) {
   }
+  
+    ngOnInit(): void {
+      this.ingredientsToAdd= [];
+      this.shoppingListToEdit = this.navParams.data.shoppingListToEdit;
+      if(this.navParams.data.type == "edit"){
+        this.editionMode = true;
+      }else{
+        this.editionMode = false;
+      }
+    }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad EditShoppingListPage');
-  }
+    addIngredient(): void {
+      const d = new Date();
+      d.setDate(d.getDate() + 14);
+      let ingredient: Ingredient = {id: 0, name: '', quantity: 0, unity: '', peremptionDate: d};
 
+      this.navCtrl.push(EditIngredientPage, { ingredient: ingredient, type: "add", shoppingListId: this.shoppingListToEdit.id })
+      //this.shoppingListToEdit.ingredientList.push(ingredient);
+      // this.addIngredientModal.hide();
+    }
+  
+    deleteIngredient(ingredient: Ingredient): void {
+      this.shoppingListToEdit.ingredientList.splice(this.shoppingListToEdit.ingredientList.indexOf(ingredient), 1);
+    }
+
+    addIngredientsToFridge(): void {
+      for (let i = 0; i < this.ingredientsToAdd.length; i++) {
+        this.fridgeService.addIngredient(this.ingredientsToAdd[i]);
+        this.shoppingListToEdit.ingredientList.splice(this.shoppingListToEdit.ingredientList.indexOf(this.ingredientsToAdd[i]), 1);
+      }
+      // this.confirmModal.hide();
+    }
+  
+    addAllIngredientsToFridge(): void {
+      for (let i = 0; i < this.shoppingListToEdit.ingredientList.length; i++) {
+        this.fridgeService.addIngredient(this.shoppingListToEdit.ingredientList[i]);
+      }
+      this.shoppingListService.removeShoppingList(this.shoppingListToEdit);
+      this.shoppingListToEdit = null;
+      // this.confirmModal.hide();
+    }
+
+    selectIngredient(e, ingredient) {
+      if (e.target.checked) {
+        this.ingredientsToAdd.push(ingredient);
+      }else {
+        this.ingredientsToAdd.splice(this.ingredientsToAdd.indexOf(ingredient), 1);
+      }
+    }
+
+    validate(){
+      this.navCtrl.pop();
+    }
 }
